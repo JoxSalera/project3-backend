0bdab0d873b828c594d50f7190d1831554ef2341
lb2718 <98560887+lb2718@users.noreply.github.com>
2022-04-14 15:21:21 +0200

    index routes logic done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,13 +6,25 @@ const User = require("../models/User.model");
 
 // GET itineraries & populate the tags and users
 router.get("/", async (req, res, next) => {
-  const itineraries = await Itinerary.find().populate("creator tags");
-  res.json(itineraries);
+  try {
+    const itineraries = await Itinerary.find().populate("creator tags");
+    res.json(itineraries);
+  } catch (err) {
+    console.log(err, "ERROR ON HOME ROUTE!");
+  }
 });
 
-router.get("/itinerary", async (req, res, next) => {
-  const itineraryDetails = await ItineraryItem.find().populate("itinerary");
-  res.json(itineraryDetails);
+router.get("/itinerary/:itineraryId", async (req, res, next) => {
+  try {
+    const { itineraryId } = req.params;
+    const itineraryDetails = await ItineraryItem.find({
+      itinerary: itineraryId,
+    }).populate("itinerary");
+    console.log(itineraryDetails);
+    res.json(itineraryDetails);
+  } catch (err) {
+    console.log(err, "ERROR ON ITINERARY/ID PAGE!");
+  }
 });
 
 module.exports = router;
