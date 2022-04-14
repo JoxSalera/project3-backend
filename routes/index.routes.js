9597fb346302db2ea909f8b428bb8e84b0a7fb5c
lb2718 <98560887+lb2718@users.noreply.github.com>
2022-04-14 11:25:35 +0200

    routes still in progress

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,16 +1,18 @@
 const router = require("express").Router();
 const Itinerary = require("../models/Itinerary.model");
+const ItineraryItem = require("../models/ItineraryItem.model");
 const Tag = require("../models/Tag.model");
 const User = require("../models/User.model");
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
-
 // GET itineraries & populate the tags and users
-router.get("/itinerary", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   const itineraries = await Itinerary.find().populate("creator tags");
   res.json(itineraries);
+});
+
+router.get("/itinerary", async (req, res, next) => {
+  const itineraryDetails = await ItineraryItem.find().populate("itinerary");
+  res.json(itineraryDetails);
 });
 
 module.exports = router;
